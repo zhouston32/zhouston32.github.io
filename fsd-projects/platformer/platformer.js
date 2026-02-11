13aa68987fca48fc0d2bf63c83f9aec3421a7b7c
zhouston32 <cxshoutslim@gmail.com>
2026-02-11 14:24:53 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,38 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     //toggleGrid();
 
 
     // TODO 2 - Create Platforms
+createPlatform(100, 610, 100, 100)
+createPlatform(200, 500, 100, 100)
+createPlatform(300, 400, 100, 100)
+createPlatform(400, 300, 100, 100)
+createPlatform(600, 300, 100, 100)
+createPlatform(800, 300, 100, 100)
+createPlatform(200, 200, 100, 100)
+createPlatform(1000, 300, 100, 100)
+createPlatform(0, 100, 100, 10)
+createPlatform(100, 100, 10, 400)
+createPlatform(1100, 200, 100, 100)
 
 
 
 
     // TODO 3 - Create Collectables
-
+createCollectable("diamond", 1330, 130)
+createCollectable("diamond", 700, 100)
+createCollectable("diamond", 130, 100)
+createCollectable("diamond", 400, 100)
 
 
     
     // TODO 4 - Create Cannons
-
+createCannon("top", 100, 500, 100, 4000)
+createCannon("right", 500, 500, 4000, 100)
+createCannon("bottom", 700, 7000, 10000, 1500)
+createCannon("top", 800, 6000, 100, 4000)
 
     
     
